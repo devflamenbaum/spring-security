beaebe54a513774e8b301fa83add4328b96251ef
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2022-03-28 14:24:14 -0300

    Resources and Final Project

---
+++ Oauth-resource-server/src/main/java/com/codewithdevflamen/oauthresource/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.codewithdevflamen.oauthresource.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/users")
+    public String[] getUser() {
+        return new String[]{"Gabriel", "Julia","Eduardo"};
+    }
+}
